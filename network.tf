cbbd4dded73eaaa6128bd976e8a3ed1a7ee0ae96
VenuGopalPentyala <venugopalpentyalaa@gmail.com>
2024-01-11 04:07:07 +0000

    Added network module

--- network.tf
+++ network.tf
@@ -1,6 +1,19 @@
 module "network" {
-  source  = "Azure/network/azurerm"
-  version = ">=3.41.0"
-  # insert the 2 required variables here
-  resource_group_name = azurerm_resource_group.myresourcegroup.location
+  source              = "Azure/network/azurerm"
+  resource_group_name = azurerm_resource_group.example.name
+  address_spaces      = ["10.0.0.0/16", "10.2.0.0/16"]
+  subnet_prefixes     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  subnet_names        = ["subnet1", "subnet2", "subnet3"]
+
+  subnet_service_endpoints = {
+    "subnet1" : ["Microsoft.Sql"],
+    "subnet2" : ["Microsoft.Sql"],
+    "subnet3" : ["Microsoft.Sql"]
+  }
+  use_for_each = true
+  tags = {
+    environment = "dev"
+    costcenter  = "it"
+  }
+
 }
