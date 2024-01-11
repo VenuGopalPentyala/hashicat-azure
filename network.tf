dcbd45b17b455d66a19daf3fdc39613af8e14bf4
VenuGopalPentyala <venugopalpentyalaa@gmail.com>
2024-01-11 04:00:23 +0000

    Added network module

--- network.tf
+++ network.tf
@@ -1,6 +1,6 @@
 module "network" {
   source  = "Azure/network/azurerm"
-  version = ">= 1.3"
+  version = ">=3.41.0"
   # insert the 2 required variables here
   resource_group_name = azurerm_resource_group.myresourcegroup.location
 }
